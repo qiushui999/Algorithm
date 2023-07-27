42c3a8527de252d9de75693e344d0d45d4268dcb
qiushui999 <2276924443@qq.com>
2023-07-27 16:29:45 +0800

    环境配置

---
+++ test.cpp
@@ -0,0 +1,14 @@
+// 这是一个调试测试文件，您可以直接运行检测配置是否生效
+// 若配置任然无法调试，请查阅README_inProject.md
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    cout<<"hello"<<endl;
+
+    system("pause");
+    return 0;
+}
